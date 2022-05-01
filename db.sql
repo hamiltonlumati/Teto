1eefeb625ff110aa118fd58f6068bf7f226a0c6c
Hamilton Lumati <hamiltonlumati@gmail.com>
2022-05-01 23:37:18 +0100

    make it better

--- db.sql
+++ db.sql
@@ -1,3 +1,12 @@
+CREATE TABLE users(
+    ID INT NOT NULL UNIQUE,
+    name CHARACTER VARYING,
+    email CHARACTER VARYING UNIQUE,
+    remember_token CHARACTER VARYING,
+    PRIMARY KEY(ID)
+
+);
+
 create table Casa(
     casaId INT NOT NULL,
     casaNome CHARACTER VARYING(40),
@@ -5,9 +14,9 @@ create table Casa(
     imagens TEXT,
     localizacao TEXT,
     PRIMARY KEY (casaId),
-    criadorID INTEGER REFERENCES users (id),
+    criadorID INTEGER REFERENCES users (ID),
     estado INT NOT NULL,
     dataCriada TIMESTAMP (3),
     Contacto INT,
     Preco money
-)
+);
