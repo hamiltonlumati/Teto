99f48a06344f568a694f0fc9659aeaa29562a0b0
Hamilton Lumati <hamiltonlumati@gmail.com>
2022-05-03 06:04:23 +0100

    Auth

--- db.sql
+++ db.sql
@@ -1,24 +1,28 @@
-/* CREATE TABLE users(
-    ID INT NOT NULL UNIQUE,
+CREATE TABLE users(
+    id BIGSERIAL NOT NULL UNIQUE ,
     name CHARACTER VARYING,
-    email CHARACTER VARYING UNIQUE,
+    email CHARACTER VARYING NOT NULL UNIQUE,
     remember_token CHARACTER VARYING,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email_verified_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    password VARCHAR NOT NULL,
     PRIMARY KEY(ID)
-
 );
 
-create table Casa(
-    casaId INT NOT NULL,
+create table casas(
+    casaId BIGSERIAL NOT NULL UNIQUE,
     casaNome CHARACTER VARYING(40),
     Divisoes TEXT,
     imagens TEXT,
     localizacao TEXT,
     PRIMARY KEY (casaId),
-    criadorID INTEGER REFERENCES users (ID),
+    criadorID BIGINT REFERENCES users (ID),
     estado INT NOT NULL,
-    dataCriada TIMESTAMP (3),
+    dataCriada TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     Contacto INT,
     Preco money
-); */
-ALTER TABLE users
-    add PASSWORD CHARACTER VARYING;
+);
+
+
+
