82c9d5ecf424b579a0752b9ab281ee80df9c0f63
Hamilton Lumati <hamiltonlumati@gmail.com>
2022-05-08 11:25:02 +0100

    make it better

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/login', function (){
+    return view('login');
 });
 
 Route::get('/dashboard', function () {
