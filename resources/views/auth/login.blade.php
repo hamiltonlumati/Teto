d1248ea8e1c420c65a740e8e0fab5df7ca0fe283
Hamilton Lumati <hamiltonlumati@gmail.com>
2022-05-09 10:37:59 +0100

    Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,60 @@
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-label for="email" :value="__('Email')" />
+
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
+
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+            </div>
+
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+                <x-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
+        </form>
+    </x-auth-card>
+</x-guest-layout>
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -15,22 +72,22 @@
     <h1 style="display: block;
                 width: 100%;
                 text-align: center">TETO</h1>
-    <form action="/login" method="POST" class="mx-auto px-auto card" style="width: 26rem;">
+    <form action="/login" method="POST" class="mx-auto px-auto card" style="width: 29rem;">
         @csrf
         <div class="card-body fs-6">
             <div class="mb-3">
-                <label for="formGroupExampleInput" class="form-label mx-auto">Email</label>
-                <input type="email" class="form-control" id="formGroupExampleInput" placeholder="nomedousuario@email.com">
+                <label for="email" class="form-label mx-auto">Email</label>
+                <input type="email" required="required" autofocus="autofocus" class="form-control" id="email" placeholder="nomedousuario@email.com">
             </div>
             <div class="mb-3">
-                <label for="formGroupExampleInput2" class="form-label">Password</label>
-                <input type="password" class="form-control" id="formGroupExampleInput2">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" id="password" required="required" autocomplete="current-password"  class="form-control" id="formGroupExampleInput2">
             </div>
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1" name="remember">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" name="remember" id="remember_me">
                 <label class="form-check-label" for="exampleCheck1">Manter-me Logado</label>
             </div>
-            <div class="mb-3 text-end">
+            <div class="mb-3 justify-content-end">
                 <a href="/forgot-password" class="mr-3">Esqueceu-se da Palavra-passe?</a>
                 <button type="submit" class="btn btn-dark">Entrar</button>
             </div>
